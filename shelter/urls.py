b2b41a8362237971f325ee4003229edc9d649c1a
Shanera D <sbeasleydavis@ivytech.edu>
2025-12-18 20:51:50 -0500

    Add employee dashboard and intake form UI

--- shelter/urls.py
+++ shelter/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('animals/', views.animals, name='animals'),
-    path('login/', views.login, name='login'),
-]
+    path('login/', views.user_login, name='login'),
+    path('employe/login/',views.employee_login, name='employee_login'),
+    path('employee/dashboard/', views.employee_dashboard, name='employee_dashboard'),
+    ]
--- shelter/views.py
+++ shelter/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
+def employee_login(request):
+    return render(request, 'shelter/employee_login.html')
+
 def home(request):
     return render(request, 'shelter/home.html')
 
 def animals(request):
     return render(request, 'shelter/animals.html')
 
-def login(request):
+def user_login(request):
     return render(request, 'shelter/login.html')
 
+def employee_dashboard(request):
+    return render(request, 'shelter/employee_dashboard.html')
+
+def intake_form(request):
+    return render(request, 'shelter/intake_form.html')
